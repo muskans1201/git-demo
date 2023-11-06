43cc6d4c9156a648a08737dc3c1fb816b785668a
MuskanS2001 <91092878+MuskanS2001@users.noreply.github.com>
2023-11-06 10:50:37 +0530

    My first commit

---
+++ loop.py
@@ -0,0 +1,9 @@
+Marks = {"New State Mobile":1000, "Pubg Mobile":20000, "Minecraft":30000, "Roblox":40000, "GTA 5":50000}
+
+print(Marks)
+
+Marks["COD"] = 2500
+
+print(Marks)
+
+#Made By Utkarsh Singh
