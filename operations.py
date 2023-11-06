ca3e771ee884bd96e4b19d46410745eac49bbfba
MuskanS2001 <91092878+MuskanS2001@users.noreply.github.com>
2023-11-06 11:48:13 +0530

    Added a code for calculator

---
+++ operations.py
@@ -0,0 +1,18 @@
+a = input('Enter first number: ')
+operator = input('Enter operator(+, -, *, /, %): ')
+b = input('Enter second number: ')
+a = int(a)
+b = int(b)
+
+if operator == '+':
+    print(a+b)
+elif operator == '-':
+    print(a-b)
+elif operator == '*':
+    print(a*b)
+elif operator == '/':
+    print(a/b)
+elif operator == '%':
+    print(a%b)
+else:
+    print('Invalid Operation')
